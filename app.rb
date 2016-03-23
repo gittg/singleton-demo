caaaf70e20a29bee86b3094c4b9e1a036a26524d
Ivan <githubh@gmail.com>
2016-03-23 22:44:07 +0200

    Refactoring

--- app.rb
+++ app.rb
@@ -1,29 +1,5 @@
-class Logger
-  def initialize
-        @f = File.open 'log.txt', 'a'
-  end
-
-  @@x = nil
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-      return @@x
-  end
-
-  def self.say_something
-      puts 'hello'
-    end
-  
-      def log_something wat
-        @f.puts wat
-      end
-end
+require './logger'
 
 Logger.instance.log_something 'something'
 
 Logger.say_something
-
- logger = Logger.new
- logger.log_something 'wazzzup'
