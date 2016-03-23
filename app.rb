0e972143c47a71d1711cee6b980ae04ec9c70ea7
Ivan <githubh@gmail.com>
2016-03-23 22:33:15 +0200

    Include self instance

--- app.rb
+++ app.rb
@@ -3,16 +3,27 @@ class Logger
         @f = File.open 'log.txt', 'a'
   end
 
+  @@x = nil
+
+  def self.instance
+    if @@x == nil
+      @@x = Logger.new
+    end
+      return @@x
+  end
+
   def self.say_something
       puts 'hello'
     end
   
-      def self.log_something wat
+      def log_something wat
         @f.puts wat
       end
 end
 
- Logger.say_something
+Logger.instance.log_something 'something'
+
+Logger.say_something
 
  logger = Logger.new
  logger.log_something 'wazzzup'
