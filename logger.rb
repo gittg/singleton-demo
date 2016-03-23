8becd08844837a0fe20b642e96cedb791f245b28
Ivan <githubh@gmail.com>
2016-03-23 22:46:18 +0200

    Refactoring

--- logger.rb
+++ logger.rb
@@ -1,24 +1,15 @@
+require 'singleton'
+
+
 class Logger
+  include Singlton
+  
   def initialize
         @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
-
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
-      return @@x
-  end
-
-  def self.say_something
-      puts 'hello'
-    end
-  
       def log_something wat
         @f.puts wat
       end
 
-      private_class_method :new
 end
